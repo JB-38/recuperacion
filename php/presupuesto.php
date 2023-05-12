79747dae528938e333a67370fafb1feeac23d0d5
JB-38 <joseb@shocklogic.com>
2023-05-12 02:48:41 -0400

    adding new styles

--- php/presupuesto.php
+++ php/presupuesto.php
@@ -13,98 +13,65 @@ if($data === FALSE){
 }
 $total=0;
 ?>
-
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Presupuesto</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="../index.html">Inicio</a></li>
-              <li class="breadcrumb-item active">Presupuesto</li>
-              
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-    
-
     <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <!-- left column -->
-          <div class="col-md-12">
-            <!-- jquery validation -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Presupuesto</h3>
+    <section name="separator-admin">
+
+      <h2>Presupuesto</h2>
+      <!-- left column -->
+      <div>
+        <!-- jquery validation -->
+        <div style="border: 1px solid #e7e4e4; border-radius: 0.5rem; background: white;">
+              <div>
+                <h3>Ver Presupuesto</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
               <form role="form" id="quickForm" method="post" action=""> 
-                <div class="card-body">
-                  <div class="form-group">
-                    
-                    <table id="tablas" class="table table-bordered table-hover">
-                  <thead>
-                    <tr>
-                        <th>Cliente</th>
-                        <th>Recursos</th>
-                        <th>Cantidad</th>
-                        <th>Fecha</th>
-                        <th>Hora</th>
-                        <th>Precio</th>
-                    </tr>
-                  </thead>
-                        <tbody>
-                          <tr>
-                            
-                    <?php                            
-                            foreach($data as $dat) {                                                        
-                      ?>
-                                <td><?php echo $dat['nombre']." ".$dat['apellido'] ?></td>
-                                <td><?php echo $dat['descripcion'] ?></td>
-                                <td><?php echo $dat['cantidad'] ?></td>  
-                                <td><?php echo $dat['fecha'] ?></td> 
-                                <td><?php echo $dat['hora'] ?></td> 
-                                <td><?php echo $dat['precio'] ?></td>  
-                                   
-                            </tr>
-                            <?php
-                                $total=$total+$dat['precio'];
-                                } 
-                            ?>                                
-                        </tbody>   
-                        
-                       </table>
-                       <h2>Total presupuesto: <?php echo $total."€"; ?></h2>
-                   
+                <div>
+                    <div style="padding: 2%;"> 
+                        <table>
+                              <thead>
+                                <tr>
+                                    <th>Cliente</th>
+                                    <th>Recursos</th>
+                                    <th>Cantidad</th>
+                                    <th>Fecha</th>
+                                    <th>Hora</th>
+                                    <th>Precio</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  
+                          <?php                            
+                                  foreach($data as $dat) {                                                        
+                            ?>
+                                      <td><?php echo $dat['nombre']." ".$dat['apellido'] ?></td>
+                                      <td><?php echo $dat['descripcion'] ?></td>
+                                      <td><?php echo $dat['cantidad'] ?></td>  
+                                      <td><?php echo $dat['fecha'] ?></td> 
+                                      <td><?php echo $dat['hora'] ?></td> 
+                                      <td><?php echo $dat['precio'] ?></td>  
+                                         
+                                  </tr>
+                                  <?php
+                                      $total=$total+$dat['precio'];
+                                      } 
+                                  ?>                                
+                              </tbody>   
+                              
+                        </table>
+                        <h2>Total presupuesto: <?php echo $total."€"; ?></h2>
                   </div>
-                  
                 </div>
-                <!-- /.card-body -->
-                <div class="card-footer">
-                  <a class="btn btn-danger" href="./iniciar.php">Volver</a>
-                </div>
-              </form>
-            </div>
+              <br>
+              <!-- /.card-body -->
+              <div class="card-footer">
+                <a type="back" href="./iniciar.php">Volver</a>
+              </div>
+              <br>
+            </form>
+      </div>
             <!-- /.card -->
-            </div>
-          <!--/.col (left) -->
-          <!-- right column -->
-          <div class="col-md-6">
-
-          </div>
-          <!--/.col (right) -->
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
+    </div>
+</section>
--- php/reservar.php
+++ php/reservar.php
@@ -10,8 +10,8 @@ if(!isset($_GET["id"])) exit();
 $id = $_GET["id"];
 
 include_once "conexion.php";
-$sentencia = $conexion->query("SELECT * FROM recursos WHERE  id = '$id';");
-$sentencia->execute([$id]);
+$sentencia = $conexion->query("SELECT * FROM recursos WHERE id = '$id';");
+$sentencia->execute();
 $data = $sentencia->fetch(PDO::FETCH_OBJ);
 if($data === FALSE){
 	echo "¡No existe ningún registro con ese ID!";
@@ -20,64 +20,45 @@ if($data === FALSE){
 
 ?>
 
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Reservar Recursos de Coaña </h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="../index.html">Inicio</a></li>
-              <li class="breadcrumb-item active">Reservaciones</li>
-              
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
      <!-- Main content -->
-     <section class="content">
-      <div class="container-fluid">
-        <div class="row">
+     <section name="separator-admin">
+      <h2>Reservaciones</h2>
+      <div>
           <!-- left column -->
-          <div class="col-md-12">
+          <div style="width: 100%; padding-left: 30%; padding-right: 30%;">
+            <br>
             <!-- jquery validation -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Reservaciones</h3>
+            <div style="border: 1px solid #e7e4e4; border-radius: 0.5rem; background: white;">
+              <div>
+                <h1>Reservaciones</h1>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
-              <form role="form" id="quickForm" method="post" action="abm_reservar.php"> 
-                <div class="card-body">
-                  <div class="form-group">
-                    <input type="hidden" name="id" value="<?php echo $data->id; ?>">
-                     
-                    
-                    <label for="nombre">Recurso</label>
-                    <input type="text" value="<?php echo $data->descripcion ?>" name="descripcion" class="form-control" readonly>
-                    <label>Precio</label>
-                    <input type="text" value="<?php echo $data->precio ?>" name="precio" class="form-control" readonly>
-                    <label>Límite de ocupantes</label>
-                    <input type="number" value="<?php echo $data->limite ?>" name="limite" class="form-control" readonly>
-                    <label>Cantidad de ocupantes a reservar</label>
-                    <input type="number" name="cantidad" class="form-control" required="">
-                    <label>Fecha de reservación</label>
-                    <input type="date"  name="fecha" class="form-control" required="">
-                    <label>Hora de reservación</label>
-                    <input type="time"  name="hora" class="form-control" required="">
-                    
-                        
-                </div>
+              <form role="form" id="quickForm" method="post" action="abm_reservar.php" style="padding: 20px"> 
+                <div>
+                  <div>
+                      <input type="hidden" name="id" value="<?php echo $data->id; ?>">
+
+                      <label for="nombre">Recurso</label>
+                      <input type="text" value="<?php echo $data->descripcion ?>" name="descripcion" readonly>
+                      <label>Precio</label>
+                      <input type="text" value="<?php echo $data->precio ?>" name="precio" readonly>
+                      <label>Límite de ocupantes</label>
+                      <input type="number" value="<?php echo $data->limite ?>" name="limite" readonly>
+                      <label>Cantidad de ocupantes a reservar</label>
+                      <input type="number" name="cantidad" required="">
+                      <label>Fecha de reservación</label>
+                      <input type="date" name="fecha" required="">
+                      <label>Hora de reservación</label>
+                      <input type="time" name="hora" required="">
+                  </div>
                   
                 </div>
                 <!-- /.card-body -->
+                <br>
                 <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Reservar</button>
-                  <a class="btn btn-danger" href="./reservas.php">Cancelar</a>
+                  <input type="submit" value="Reservar">
+                  <a type="back" href="./reservas.php">Cancelar</a>
                 </div>
               </form>
             </div>
@@ -88,10 +69,6 @@ if($data === FALSE){
           <div class="col-md-6">
 
           </div>
-          <!--/.col (right) -->
-        </div>
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </section>
-    <!-- /.content -->
-  </div>
